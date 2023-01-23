770d1b179fedd579f52ae8c57f58d91fd2158738
AYehia0 <ahmedyehia611@gmail.com>
2023-01-23 21:42:16 +0200

    Started working with the data
    - Parsing the JSON data.
    - Getting Ayaht in a certain page.

---
+++ pkg/quran/quran.go
@@ -0,0 +1,47 @@
+package quran
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const (
+	SURAH_NUM = 114 // the number of surahs in the holy quran
+	LANG      = 0   // AR -> 0, EN -> 1
+)
+
+// get all the ayaht in a particular page by scanning the pages for a certain page number
+func AyahtInPage(surahs *[][]Surah, pageNum int) []*Ayah {
+	ayaht := make([]*Ayah, 0)
+	for _, surah := range *surahs {
+		for _, ayah := range surah[LANG].Ayaht {
+			if ayah.Page == pageNum {
+				ayaht = append(ayaht, &ayah)
+			} else {
+				break
+			}
+		}
+	}
+	return ayaht
+}
+
+func ParseQuranData(dataFilePath string) *[][]Surah {
+
+	// the first index for surah number/order in mushaf, the second is either 0,1 (ar, en)
+	chapters := [][]Surah{}
+
+	f, err := os.Open(dataFilePath)
+	if err != nil {
+		fmt.Printf("Error reading the data! : %v\n", err)
+		os.Exit(1)
+	}
+
+	data := json.NewDecoder(f)
+	if err := data.Decode(&chapters); err != nil {
+		fmt.Printf("Error decoding the file! : %v\n", err)
+		os.Exit(1)
+	}
+	return &chapters
+
+}
---
+++ pkg/quran/types.go
@@ -0,0 +1,18 @@
+package quran
+
+type Surah struct {
+	NumberSurah int    `json:"number"`
+	NameAr      string `json:"name"`
+	NameEn      string `json:"englishName"`
+	NumberAyaht int    `json:"numberOfAyahs"`
+	Ayaht       []Ayah `json:"ayahs"`
+}
+
+type Ayah struct {
+	Text          string `json:"text"`
+	NumberInSurah int    `json:"numberInSurah"`
+	NumberInQuran int    `json:"number"`
+	Juz           int    `json:"juz"`
+	Verse         int    `json:"chapter"`
+	Page          int    `json:"page"`
+}
